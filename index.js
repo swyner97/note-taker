560a1fb9a4541023a5cf8228f73518559cb858bc
swyner97 <swyner97@gmail.com>
2022-10-11 15:49:42 -0700

    unique id

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
-const path = require('path');
-// npm run dev
+const fs = require('fs');
+const uniqid = require('uniqid')
+const { reset } = require('nodemon');
+
 const app = express();
 
-// Set static folder 
+app.use(express.static('public'));
+app.use(express.json());
+app.get(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// npm run dev
+
+app.get('/api/notes', (req, res) => {
+    const notes = require('./db/notes.json');
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = require('./db/notes.json');
+    const newNote = {
+        id: uniqid(),
+        title: req.body,
+        note: req.body
+    }
+    notes.push(newNote);
+
+    fs.writeFile('./db/notes.json', JSON.stringify(notes), (err) => {
+        if (err) {
+            res.status(500).end();
+        } else {
+            res.status(200).json({
+                message: 'Everything went ok!'
+            })
+        }
+    })
+} )
+
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
