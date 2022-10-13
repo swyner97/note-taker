89802dbe9683104ff7d4262ecdeaf79cf5dba9a1
swyner97 <swyner97@gmail.com>
2022-10-13 14:14:16 -0700

    need to return notes.html

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-const uniqid = require('uniqid')
+const path = require('path');
+const uniqid = require('uniqid');
 const { reset } = require('nodemon');
-
-const idFilter = req => note => note.id === parseInt(req.params.id);
 
 const app = express();
 
@@ -18,19 +17,25 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.get('*', (req, res) => {
-    const notes = require('./db/notes.json');
-    res.json(notes)
+app.get('/notes', (req,res) => {
+    res.readFile('notes.html')
+    // .then(contents => {
+    //     res.setHeader("Content-Type", "text/html");
+    //     res.writeHead(200);
+    //     res.end(contents);
+    // })
+    // .catch(err => {
+    //     res.writeHead(500);
+    //     res.end(err);
+    //     return;
+    // });
 })
 
 app.post('/api/notes', (req, res) => {
     const notes = require('./db/notes.json');
-    const newNote = {
-        id: uniqid(),
-        title: req.body,
-        note: req.body
-    }
-    notes.push(newNote);
+    const id = uniqid();
+    const { title, text } = req.body;
+    notes.push({ title, text, id });
 
     fs.writeFile('./db/notes.json', JSON.stringify(notes), (err) => {
         if (err) {
@@ -43,34 +48,25 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
-// app.get('/api/notes/:id', (req, res) => {
-//     const notes = require('./db/notes.json');
-//     const found = notes.some(idFilter(req));
 
-//     if (found) {
-//         res.json(notes.filter(idFilter(req)));
+// app.delete("/api/notes/:id", (req, res) => {
+//     const notes = require("./db/notes.json");
+
+//     // grab the note by comparing ids
+//     const noteToDelete = notes.find(note => note.id === req.params.id);
+//     console.log(noteToDelete);
+//     if (noteToDelete) {
+//         res.json({ deleted: noteToDelete });
 //     } else {
-//         res.status(400).json({ msg: `No notes with the id of ${req.params.id}` });
+//         res.status(400).json({ msg: `No note with the id of ${req.params.id}` });
 //     }
 
+
+//     if (!fs.existsSync(DB_DIR)) {
+//         fs.mkdirSync(DB_DIR);
+//     }
+//     fs.writeFileSync(dbPath, generateJSON(notes))
 // });
-
-
-app.delete('/api/notes/:id', (req, res) => {
-    const notes = require('./db/notes.json');
-    const found = notes.some(idFilter(req));
-
-    if (found) {
-        res.json({
-            msg: 'Note deleted.',
-            notes: notes.filter(note => !idFilter(req)(note))
-        });
-    } else {
-        res.status(400).json({ msg: `No note with the id of ${req.params.id}` });
-    }
-
-})
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
