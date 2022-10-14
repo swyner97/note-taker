7b5832ce9cc0451053d7928d65c3837c3d8991cc
swyner97 <swyner97@gmail.com>
2022-10-13 19:17:04 -0700

    fixed get * bug

--- index.js
+++ index.js
@@ -16,19 +16,15 @@ app.get(express.urlencoded({ extended: true }));
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
+});
 
-// GET * should return the index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
     const notes = require('./db/notes.json');
     res.json(notes);
 
-})
+});
 
 app.post('/api/notes', (req, res) => {
     const notes = require('./db/notes.json');
@@ -46,7 +42,12 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.error('Error adding note')
     }
-})
+});
+
+// GET * should return the index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 // app.delete("/api/notes/:id", (req, res) => {
 //     const notes = require("./db/notes.json");
