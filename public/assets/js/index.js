2028132c72931fc138a4bf9e7f102dc7484d4e29
swyner97 <swyner97@gmail.com>
2022-10-19 19:37:13 -0700

    fixed function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -96,7 +96,7 @@ const handleNoteDelete = (e) => {
 };
 
 // Sets the activeNote and displays it
-const handleNoteView = (e) => {
+const ew = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -7,8 +7,8 @@ const uniqid = require('uniqid');
 
 
 // GET /notes should return the notes.html file.
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__filename, '../../public/notes.html'));
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -27,7 +27,7 @@ router.post('/api/notes', (req, res) => {
             id: uniqid()
         }
 
-        readAndrouterend(newNote, '../db/notes.json');
+        readAndAppend(newNote, '../db/notes.json');
         res.json(`Note successfully added`);
     } else {
         res.error('Error adding note')
