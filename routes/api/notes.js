734e1bf9c7537825156b3296a46e63977e0df258
swyner97 <swyner97@gmail.com>
2022-10-19 19:45:10 -0700

    fixed some paths

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const { readAndAppend } = require('../../helpers/fsUtils');
 const notes = require('../../db/notes.json');
 const uniqid = require('uniqid');
 
